0825b52a696046f3fa909a14aad37be7be228217
baojie1981 <1039245098@qq.com>
2019-05-11 10:58:37 +0800

    队列初始化太小会出现bug。读取数据为空，读不出数据。

--- src/main/java/com/baojie/fbq/TestQueue.java
+++ src/main/java/com/baojie/fbq/TestQueue.java
@@ -4,52 +4,47 @@ import com.baojie.fbq.bytes.LocalBytes;
 
 public class TestQueue {
 
-    public static void main(String args[]) throws Exception{
-        FQueue fQueue=new FQueue("/home/baojie/liuxin/source/fileblockqueue",20);
+    public static void main(String args[]) throws Exception {
+        //FQueue fQueue = new FQueue("/home/baojie/liuxin/source/fileblockqueue", 200);
+        FQueue fQueue = new FQueue("/home/baojie/liuxin/source/fileblockqueue", 20);
 
-        for(int i=0;i<20;i++){
-            byte[] one= LocalBytes.seria(new Runer(i+""));
-
+        for (int i = 0; i < 20; i++) {
+            byte[] one = LocalBytes.seria(new Runer(i + ""));
             fQueue.offer(one);
-
         }
-        int i=0;
-        for(;;){
-            byte[] one= fQueue.poll();
-            if(null!=one){
-                i++;
-                Runer runer=LocalBytes.deseria(one,Runer.class);
+        int i = 0;
+        for (; ; ) {
+            byte[] one = fQueue.poll();
+            i++;
+            if (null != one) {
+                Runer runer = LocalBytes.deseria(one, Runer.class);
                 System.out.println(runer.getInfo());
                 runer.run();
-            }else {
-                break;
+            } else {
+                if (i >= 20) {
+                    break;
+                }
             }
-
         }
-
-        //System.out.println(i);
-
+        // System.out.println(i);
         fQueue.close();
-
-
-
 
     }
 
-    public static final class Runer implements Runnable{
+    public static final class Runer implements Runnable {
 
         private final String info;
 
-        public Runer(String info){
-            this.info=info;
+        public Runer(String info) {
+            this.info = info;
         }
 
         @Override
-        public void run(){
+        public void run() {
             System.out.println(getInfo());
         }
 
-        public String getInfo(){
+        public String getInfo() {
             return info;
         }
 
